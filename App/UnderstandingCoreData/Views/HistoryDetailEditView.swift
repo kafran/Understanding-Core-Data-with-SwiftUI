0d8768bd16e4125bcfe25d5ac51dc3256d47bf9f
Kolmar Kafran <kolmar@kafran.net>
2022-11-25 08:53:23 -0300

    History add/edit views

---
+++ App/UnderstandingCoreData/Views/HistoryDetailEditView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryDetailEditView.swift
+//  UnderstandingCoreData
+//
+//  Created by Kolmar Kafran on 24/11/22.
+//
+
+import SwiftUI
+
+struct HistoryDetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryDetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryDetailEditView()
+    }
+}
